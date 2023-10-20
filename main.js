4536ee7c01284a003782b6e3760898b80a1fdf46
Abdullah0abbas <abdullah000abbas@gmail.com>
2023-10-21 00:37:37 +0300

    new

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ ScrollReveal({
     duration: 2000,
     delay: 200
 });
+
+
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about img', { origin: 'left' });
