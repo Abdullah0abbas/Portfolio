054c120835a44ff373e21651f310539a2064fa24
Abdullah0abbas <abdullah000abbas@gmail.com>
2023-10-21 00:29:34 +0300

    new project

--- main.js
+++ main.js
@@ -52,4 +52,16 @@ const typed = new typed('.multiple-text', {
 
 });
 
-
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
